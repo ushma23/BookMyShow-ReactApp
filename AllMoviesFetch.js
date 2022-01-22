54f160a705c88de2813e48114d65c2b84756c1c1
Shaik Ushma <81610823+ushma23@users.noreply.github.com>
2022-01-22 17:17:31 +0530

    Add files via upload

---
+++ AllMoviesFetch.js
@@ -0,0 +1,43 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import axios from "axios";
+import {useState, useEffect} from "react";
+//import data from "./movies.json";
+
+function AllMoviesFetch() {
+    const [data, setData] = useState([]);
+
+    useEffect(async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/movies');
+            console.log(response);
+            setData(response.data);
+        } catch (error) {
+            console.error(error); 
+        }
+    }, []);
+
+    return (
+        <div>
+            <Container fluid style={{padding:"5%", background:"pink"}}>
+                <Row style={{textAlign:"center"}}>
+                    {data.map((mov) =>
+                        <Col onClick={()=>window.location.href="/movies/"+mov.id} id={mov.id} key={mov.id} xs={6} md={4} lg={3} style={{marginBottom:"2%"}}>
+                            <Card style={{cursor:"pointer"}} onClick={()=> window.location.href="/movies/"+mov.id }>
+                                <Card.Img variant="top" src={mov.image} />
+                                <Card.Body>
+                                <Card.Title>{mov.title}</Card.Title>
+                                <Card.Text>{mov.actor}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )}
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default AllMoviesFetch;
